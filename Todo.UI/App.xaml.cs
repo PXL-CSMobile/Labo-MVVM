26ed137ec84bb5ee0d5195ce7da92be499649678
Wim Bervoets <wim.bervoets@pxl.be>
2024-11-01 20:32:49 +0100

    Add project files.

---
+++ Todo.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Todo.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
